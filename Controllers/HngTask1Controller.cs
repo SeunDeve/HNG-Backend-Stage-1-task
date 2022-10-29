1720dd332479aa0d1a069a54307ec49532d04be4
SeunDeve <eseun.oyewole@gmail.com>
2022-10-29 09:53:29 +0100

    add project file

---
+++ Controllers/HngTask1Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hngbackend.Model;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace Hngbackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HngTask1Controller : Controller
+    {
+        [HttpGet]
+        public IActionResult GetUserDetails()
+        {
+            var res = new Response()
+            {
+                slackUsername = "Oluwaseun Oyewole",
+                backend = true,
+                age = 26,
+                bio = "Tech Bro"
+            };
+
+            return Ok(res);
+        }
+
+    }
+}
+
--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
@@ -10,11 +6,18 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Hngbackend
 {
     public class Startup
     {
+
+        public readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,6 +28,23 @@ namespace Hngbackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  policy =>
+                                  {
+                                      policy.AllowAnyOrigin();
+                                      policy.AllowAnyHeader();
+                                      policy.AllowAnyMethod();
+                                  });
+            });
+
+            //services.AddCors(options =>
+            //    options.AddPolicy("MyCorsPolicy",
+            //        builder => builder.WithOrigins(Configuration["AllowedCorsOrigin"].Split(",", StringSplitOptions.RemoveEmptyEntries))
+            //        .WithMethods
+            //        ("GET")));
+
             services.AddControllers();
         }
 
@@ -36,12 +56,20 @@ namespace Hngbackend
                 app.UseDeveloperExceptionPage();
             }
 
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseCors(MyAllowSpecificOrigins);
+
             app.UseAuthorization();
 
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("access-control-allow-origin", "*");
+                await next();
+            });
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
@@ -49,4 +77,3 @@ namespace Hngbackend
         }
     }
 }
-
