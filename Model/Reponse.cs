b88d54cc64831e8c322573ed5345d8eed87a0c93
SeunDeve <eseun.oyewole@gmail.com>
2022-10-29 09:51:59 +0100

    add model

---
+++ Model/Reponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hngbackend.Model
+{
+    public class Response
+    {
+        public string slackUsername { get; set; }
+
+        public bool backend { get; set; }
+
+        public int age { get; set; }
+
+        public string bio { get; set; }
+    }
+}
+
+
